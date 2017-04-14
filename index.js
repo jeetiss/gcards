6742970e7ffadce7d027dec85522f36ea512ff58
Dmitry <za5rubley@gmail.com>
2017-04-14 11:47:28 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const { createServer } = require('http')
+
+const range = (from, to) => Array.from(
+  {length: to - from + 1},
+  (_, index) => index + from
+)
+
+const random = (from, to) => Math.round(
+  Math.random() * (to - from)
+) + from
+
+const height = 200
+const partUrl = 'http://placehold.it/'
+
+const generateCards = () => range(1, random(10, 25)).map(number => {
+  const size = `${height}x${random(150, 250)}`
+  const url = `${partUrl}${size}?text=${number}`
+
+  return { size, url }
+})
+
+const PORT = process.env.PORT || 5000
+const server = createServer((_, res) => {
+  const content = JSON.stringify(generateCards())
+
+  res.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  })
+
+  setTimeout(() => {
+    res.end(content)
+  }, 2000)
+})
+
+server.listen(5000, () => {
+  console.log(`server work on ${PORT}`)
+})
